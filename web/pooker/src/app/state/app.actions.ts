ae692b4ea17c45e7bbbcee8fbac1fc95707db05b
vikas-raj <43149129+vikas-raj@users.noreply.github.com>
2021-06-01 18:50:07 +0530

    Update app.actions.ts

--- web/pooker/src/app/state/app.actions.ts
+++ web/pooker/src/app/state/app.actions.ts
@@ -1,1 +1,10 @@
 
+
+import { createAction, props } from '@ngrx/store';
+
+export const getUser = createAction(
+  '[App Component] Get Assignment Details',
+  props<{ id: number }>());
+export const setUser = createAction(
+  '[App Component] Set Assignment Details',
+  props<{ data: any }>());
