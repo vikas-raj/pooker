51e2f2e719b28e3df384f7e774201feb2a035156
vikas-raj <43149129+vikas-raj@users.noreply.github.com>
2021-06-01 12:25:07 +0530

    Update app.module.ts

--- web/pooker/src/app/app.module.ts
+++ web/pooker/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
 
+import { reducers, metaReducers } from './state';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppEffects } from './state/app.effects';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    EffectsModule.forRoot([AppEffects]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
