e920b7a260bb29e3fa7bb9a55624b32f59ac8d11
vikas-raj <43149129+vikas-raj@users.noreply.github.com>
2021-06-01 12:32:09 +0530

    Update index.ts

--- web/pooker/src/app/state/index.ts
+++ web/pooker/src/app/state/index.ts
@@ -1,4 +1,5 @@
-
+import { ActionReducerMap, createSelector, MetaReducer } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../environments/environment';
 
 export interface IAppState {
